7f9d42c7326d2060582a3be193380590565ac444
user <khantmaw48.com>
2023-03-14 10:45:22 +0630

    remove Box

--- src/pages/Header/HeaderFooter.jsx
+++ src/pages/Header/HeaderFooter.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Grid, Typography } from '@mui/material'
+import { Button, Grid, Typography } from '@mui/material'
 import React from 'react'
 // import tablet from '../../images/Tablet.png'
 import Style from './Style.module.css'
